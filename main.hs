ce5c30a970393eb5e17677a65f2b8b7ae1d09928
Sahaswari Senanayaka <smsahaswari@gmail.com>
2025-09-19 05:56:30 +0530

    Improve user prompts for coefficient input in quadratic equation solver

--- main.hs
+++ main.hs
@@ -1,12 +1,16 @@
 main :: IO ()
 main = do
-    putStrLn "Enter coefficients a, b, and c:"
+    putStrLn "Enter coefficients a, b, and c. Add a :"
     input1 <- getLine
+    putStrLn "Add b:"
     input2 <- getLine
+    putStrLn "Add c:"
     input3 <- getLine
+
     let a = read input1 :: Double
         b = read input2 :: Double
         c = read input3 :: Double
+        
     let discriminant = b^2 - 4*a*c
     if discriminant > 0 then
         let root1 = (-b + sqrt discriminant) / (2*a)
